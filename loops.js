bc7fe9920c94ac36517d65885588b592dde9e4ea
Rhea <rhea.gomez1401@gmail.com>
2024-05-06 14:34:02 +0530

    Learnt for & while loops

---
+++ loops.js
@@ -0,0 +1,38 @@
+//using for and while loops
+//task1
+for (var i = 1; i < 6; i++){
+    console.log(i);
+}
+console.log("Counting completed!");
+
+//task2
+for (var i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log("Countdown finished!");
+
+//task3
+var i = 1;
+
+while (i < 6) {
+    console.log(i);
+    i++;
+}
+console.log("Counting completed!");
+
+//task 4
+var i = 5;
+
+while (i > 0) {
+    console.log(i);
+    i = i - 1;
+}
+console.log("Countdown finished!");
+
+//task 5
+var i = 2018;
+
+while (i < 2023) {
+    console.log(i);
+    i++;
+}
